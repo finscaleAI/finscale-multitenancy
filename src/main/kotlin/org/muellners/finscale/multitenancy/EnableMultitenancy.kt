66d88c32f400bda03943d2a9e60203909e552b42
Kevin Madhu <kevin.madhu@gmail.com>
2020-07-16 16:07:01 +0530

    Adds target type for EnableMultitenany annotation.

--- src/main/kotlin/org/muellners/finscale/multitenancy/EnableMultitenancy.kt
+++ src/main/kotlin/org/muellners/finscale/multitenancy/EnableMultitenancy.kt
@@ -3,7 +3,7 @@ package org.muellners.finscale.multitenancy
 import org.muellners.finscale.multitenancy.config.MultitenancyImportSelector
 import org.springframework.context.annotation.Import
 
-@kotlin.annotation.Target
+@kotlin.annotation.Target(AnnotationTarget.TYPE)
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 @Import(MultitenancyImportSelector::class)
 annotation class EnableMultitenancy()
